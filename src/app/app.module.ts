16695d1729cc2025b56ebf8884bf4c7eb5340d2a
Jose Gabriel Alvarado Perez <e18470428@itcampeche.edu.mx>
2020-06-08 17:32:34 -0500

    se agrego el componente summary

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { SummaryComponent } from './components/summary/summary.component';
 @NgModule({
 declarations: [
-AppComponent
+AppComponent,
+SummaryComponent
 ],
 imports: [
 BrowserModule,
